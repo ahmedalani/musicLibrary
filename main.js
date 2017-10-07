cfd82484fbbffb3bc9b56bae5e1cf09814d8ff11
Ahmed Alani <s3leha@gmail.com>
2017-10-07 22:03:46 +0000

    cleaned code from comments

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// function rating() {
-//   return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-// }
-
 var Library = function (name, creator) {
   this.name = name;
   this.creator = creator;
@@ -42,59 +38,9 @@ Playlist.prototype.addTrack = function (trk) {
 }
 
 
-
-const bobLibrary = new Library('bob', 'joe');
-const testPlaylist = new Playlist("test");
-const testTrack = new Track("testTrack", 3, 2);
-testPlaylist.addTrack(testTrack);
-bobLibrary.addPlaylist(testPlaylist);
-console.log(JSON.stringify(bobLibrary));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var SmartShoe = function (size, colour, type) {
-//   this.size = size;
-//   this.colour = colour;
-//   this.type = type;
-//   // things never arrive with full battery power ;)
-//   this.battery = 20;
-// }
-
-// // define a prototype (~instance) method
-// SmartShoe.prototype.recharge = function (powerIncrease) {
-//   this.battery += powerIncrease;
-//   if (this.battery > 100) {
-//     this.battery = 100;
-//   }
-// }
-
-// // create a new object called `nikeShoe` with the SmartShoe constructor
-// var nikeShoe = new SmartShoe(10, 'gray', 'sport edition');
-// // we call the prototype method recharge
-// nikeShoe.recharge(50);
+// const bobLibrary = new Library('bob', 'joe');
+// const testPlaylist = new Playlist("test");
+// const testTrack = new Track("testTrack", 3, 2);
+// testPlaylist.addTrack(testTrack);
+// bobLibrary.addPlaylist(testPlaylist);
+// console.log(JSON.stringify(bobLibrary));
